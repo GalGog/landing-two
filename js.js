3ac62ad2bebae32ba408a94e8404b852c5826b0c
GalGog <galynna00@gmail.com>
2018-03-14 18:56:55 +0000

    landing two

---
+++ js.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+
+    var guestAmount = $('#guestNo');
+
+    $('#cnt-up').click(function() {
+        guestAmount.val(Math.min(parseInt($('#guestNo').val()) + 1, 20));
+    });
+    $('#cnt-down').click(function() {
+        guestAmount.val(Math.max(parseInt($('#guestNo').val()) - 1, 1));
+    });
+
+    $('.btn').click(function() {
+
+        var $btn = $('.btn');
+
+        $btn.toggleClass('booked');
+        $('.diamond').toggleClass('windup');
+        $('form').slideToggle(300);
+        $('.linkbox').toggle(200);
+
+        if ($btn.text() === "Book Now!") {
+            $btn.text("Booked!");
+        } else {
+            $btn.text("Book Now!");
+        }
+    });
+});
+
